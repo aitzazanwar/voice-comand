8f4671e43d325c242223dcebe46b3ce105bea5ab
Accounts Coordinator 1 <acc_coordinator1@orbit.local>
2023-03-29 11:55:59 +0100

    intial code and files added

---
+++ voice-comand/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Container, Navbar, Nav, Row, Col, Button } from 'react-bootstrap';
+
+function HomePage() {
+  return (
+    <Container className="d-flex flex-column justify-content-center align-items-center vh-100">
+      <Navbar bg="light" expand="md">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#services">Services</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+            <Nav.Link href="https://netfreight.descartes.com/Login/ClientLogin.aspx">NetFreight</Nav.Link>
+            <Nav.Link href="mailto:accounts@orbitcargoservices.com">@accounts</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <Row>
+        <Col className="text-center">
+          <h1>Welcome to Voice recognizer</h1>
+          <p>This is my homepage.</p>
+          
+          <Button variant="primary">Click & Speak</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default HomePage;
